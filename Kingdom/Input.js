97dee2e538c57253c7fbd7ed23e60838b77c5fe9
Thomas Giles <wthit56@googlemail.com>
2015-12-30 14:30:42 +0000

    Kingdom
    repeating parallax images, shift to move faster, events stored with
    precision times (cleared each frame update)

--- Kingdom/Input.js
+++ Kingdom/Input.js
@@ -1,5 +1,7 @@
 var Input = {
+	events: [],
 	keyCodes: {
+		16: "shift",
 		38: "up", 40: "down",
 		37: "left", 39: "right"
 	}
@@ -8,10 +10,14 @@ var Input = {
 on(window, "keydown", function(e) {
 	if (e.keyCode in Input.keyCodes) {
 		Input[Input.keyCodes[e.keyCode]] = true;
+		Input.shift = e.shiftKey;
 	}
+	Input.events.push({ time: performance.now(), event: e });
 });
 on(window, "keyup", function(e) {
 	if (e.keyCode in Input.keyCodes) {
 		Input[Input.keyCodes[e.keyCode]] = false;
+		Input.shift = e.shiftKey;
 	}
+	Input.events.push({ time: performance.now(), event: e });
 });
--- Kingdom/game.js
+++ Kingdom/game.js
@@ -1,33 +1,40 @@
 if (!canvas) { alert("Your browser does not support canvas."); }
 else {
 	canvas.width = 864; canvas.height = 480;
-	
-	
 	var ctx = canvas.context;
+	
+	var zoom = 2;
+	var view = { width: canvas.width / zoom, height: canvas.height / zoom };
+	var camera = { x: 100, y: 0 };
+	var movementSpeed = 100 / 1000;
+	
+	ctx.scale(zoom, zoom);
 	ctx.imageSmoothingEnabled = false;
 	//ctx.fillStyle = "red"; ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-	var zoom = 2;
-	ctx.scale(zoom, zoom);
-
-	var camera = { x: 0, y: 0 };
-	var movementSpeed = 100 / 1000;
 	
 	var parallax = Loader.base("graphics/$&.png").images("skyline_hills,skyline_trees".split(",")).then(function() {
 		var hills = parallax.assets["skyline_hills"] = tintImage(parallax.assets["skyline_hills"], "#1C921C");
+		hills.name = "skyline_hills";
 		hills.distance = 5;
 		hills.top = 0;
+		hills.minX = -hills.width + view.width;
 		
 		var trees = parallax.assets["skyline_trees"] = tintImage(parallax.assets["skyline_trees"], "#234D37");
+		trees.name = "skyline_trees";
 		trees.distance = 1;
 		trees.top = 60;
+		trees.minX = -trees.width + view.width;
 		
 		parallax.draw = function() {
 			drawProp(parallax.assets["skyline_hills"]);
 			drawProp(parallax.assets["skyline_trees"]);
 		};
 		function drawProp(prop) {
-			ctx.drawImage(prop, camera.x / prop.distance, prop.top);
+			var x = camera.x / prop.distance;
+			x = x % prop.width;
+			if (x > 0) { ctx.drawImage(prop, x - prop.width, prop.top); }
+			else if (x < prop.minX) { ctx.drawImage(prop, x + prop.width, prop.top); }
+			ctx.drawImage(prop, x, prop.top);
 		}
 		
 		tick();
@@ -48,10 +55,11 @@ else {
 	};
 	var update = function(delta) {
 		if (Input.left) {
-			camera.x += delta * movementSpeed;
+			camera.x += delta * movementSpeed * (Input.shift ? 10 : 1);
 		}
 		else if (Input.right) {
-			camera.x -= delta * movementSpeed;
+			camera.x -= delta * movementSpeed * (Input.shift ? 10 : 1);
 		}
+		Input.events.length = 0;
 	};
 }
